37d125b97376a2e93514d9b8fcd9e91f6e4000e6
Tyler Singer-Clark <tscizzle@gmail.com>
2013-11-13 21:13:31 -0500

    added associations to location model

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,4 +1,9 @@
 class Location < ActiveRecord::Base
+	belongs_to :user
+
+	has_many :tag_locations
+	has_many :tags, :through => :tag_locations
+
 	geocoded_by :address
 	after_validation :geocode, :if => :address_changed?
 
