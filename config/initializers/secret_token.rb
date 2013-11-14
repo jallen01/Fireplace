dcb1f1330f21838bda9c83907ee24f6626b03ef3
Tyler Singer-Clark <tscizzle@gmail.com>
2013-11-13 19:44:48 -0500

    created rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TscizzleMichelleojKrosnickJallen01Final::Application.config.secret_key_base = 'c42f132670a84f82157ce122792d40a248a2c31acd50d1ea523be1866e90b93215340cb1b6e490e6457d69dac832e55c16e76faf31918731902e9a69a15e1097'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TscizzleMichelleojKrosnickJallen01Final::Application.config.session_store :cookie_store, key: '_tscizzle_michelleoj_krosnick_jallen01_final_session'
