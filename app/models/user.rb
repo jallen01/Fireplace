6e9b9afb388089c714c77be30be33e762ce25e2f
Jonathan Allen <jallen01@mit.edu>
2013-11-20 13:39:31 -0500

    Fix Devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -103,22 +103,4 @@ class User < ActiveRecord::Base
     real_rel_tasks
 
   end
-
-
-  # Authentication
-  # --------------
-  
-  def User.new_remember_token
-      SecureRandom.urlsafe_base64
-  end
-
-  def User.encrypt(token)
-      Digest::SHA1.hexdigest(token.to_s)
-  end 
-
-  private
-
-    def create_remember_token
-      self.remember_token = User.encrypt(User.new_remember_token)
-    end
 end
