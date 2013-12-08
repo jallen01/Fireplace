72f251a74be51b40816f5ac40a155f12fef0d509
Jonathan Allen <jallen01@mit.edu>
2013-12-08 02:44:19 -0500

    Small changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,11 +3,11 @@ class UsersController < ApplicationController
   # 'time_frame' must be in 'TIME_FRAMES'.
   def update_context_overrides
     time_frame = params[:time_frame].to_sym
-    time_frame = User::TIME_FRAMES.include?(time_frame) ? time_frame : nil
+    time_frame = User::TIME_FRAMES.include?(time_frame) ? time_frame : :now
     location = current_user.get_locations.exists?(id: params[:location]) ? params[:location] : nil
 
     session[:context_overrides] = {time_frame: time_frame, location: location}
-    
+
     @context = current_user.get_context(session[:context_overrides], session[:location], session[:utc_offset])
 
     respond_to do |format|
